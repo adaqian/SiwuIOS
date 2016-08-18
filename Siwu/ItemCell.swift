12d0147d62f155aecf13ce0754f21c9492875b67
dongqian <dongqian@rainman-2.local>
2016-08-18 16:59:23 +0800

    commit

---
+++ Siwu/ItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemCell.swift
+//  Siwu
+//
+//  Created by dongqian on 16/8/18.
+//  Copyright © 2016年 edu. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
